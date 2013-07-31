ebe32a66dab0277c50736a96559e56dd282f2b6f
k-hotta <k-hotta@ist.osaka-u.ac.jp>
2013-07-31 18:24:44 +0900

    create a class to create instances of CRDElement

---
+++ src/jp/ac/osaka_u/ist/sdl/prevol/methodanalyzer/CRDElementCalculator.java
@@ -0,0 +1,32 @@
+package jp.ac.osaka_u.ist.sdl.prevol.methodanalyzer;
+
+import jp.ac.osaka_u.ist.sdl.prevol.data.CRDElement;
+
+import org.eclipse.jdt.core.dom.ASTNode;
+
+/**
+ * CRDElement を計算するクラス
+ * 
+ * @author k-hotta
+ * 
+ */
+public class CRDElementCalculator {
+
+	/**
+	 * 与えられたノードと文字列生成器を用いて CRDElement を生成する
+	 * 
+	 * @param node
+	 * @param strBuilder
+	 * @return
+	 */
+	public static final CRDElement calc(final ASTNode node,
+			final AbstractBlockStringBuilder<?> strBuilder) {
+		final MetricsCalculator metricsCalculator = new MetricsCalculator();
+		node.accept(metricsCalculator);
+
+		final int cm = metricsCalculator.getCC() + metricsCalculator.getFO();
+
+		return new CRDElement(strBuilder.getBlockStr(), cm);
+	}
+
+}
