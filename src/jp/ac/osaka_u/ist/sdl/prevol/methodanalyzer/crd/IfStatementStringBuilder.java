be1d585674d42c9479be0f2faadf9a4fb82274f0
k-hotta <k-hotta@ist.osaka-u.ac.jp>
2013-07-31 19:01:18 +0900

    create a class to construct a string for a node of if statement

--- src/jp/ac/osaka_u/ist/sdl/prevol/methodanalyzer/crd/IfStatementStringBuilder.java
+++ src/jp/ac/osaka_u/ist/sdl/prevol/methodanalyzer/crd/IfStatementStringBuilder.java
@@ -1,6 +1,28 @@
 package jp.ac.osaka_u.ist.sdl.prevol.methodanalyzer.crd;
 
+import org.eclipse.jdt.core.dom.IfStatement;
+
+/**
+ * IfStatement に対する文字列を生成するクラス
+ * 
+ * @author k-hotta
+ * 
+ */
 public class IfStatementStringBuilder extends
 		AbstractBlockStringBuilder<IfStatement> {
+
+	public IfStatementStringBuilder(IfStatement node) {
+		super(node);
+	}
+
+	@Override
+	public String getBlockStr() {
+		final StringBuilder builder = new StringBuilder();
+		
+		builder.append("IF,"); // 文の種類
+		builder.append(node.getExpression());
+		
+		return null;
+	}
 
 }
