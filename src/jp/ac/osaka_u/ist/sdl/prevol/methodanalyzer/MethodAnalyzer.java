5e3ca8a5a45d75a05f923d90a65346fff283db00
k-hotta <k-hotta@ist.osaka-u.ac.jp>
2013-08-01 22:42:12 +0900

    implement a function to initialize repository with given option values

--- src/jp/ac/osaka_u/ist/sdl/prevol/methodanalyzer/MethodAnalyzer.java
+++ src/jp/ac/osaka_u/ist/sdl/prevol/methodanalyzer/MethodAnalyzer.java
@@ -1,5 +1,7 @@
 package jp.ac.osaka_u.ist.sdl.prevol.methodanalyzer;
 
+import jp.ac.osaka_u.ist.sdl.prevol.methodanalyzer.svn.SVNRepositoryManager;
+
 /**
  * リポジトリを解析してメソッド情報をDBに保存する機能のメインクラス
  * 
@@ -11,13 +13,17 @@ public class MethodAnalyzer {
 	public static void main(String[] args) {
 		try {
 			// 引数を解析
-			final MethodAnalyzerSettings settings = MethodAnalyzerSettings.parseArgs(args);
-			
-			
-			
+			final MethodAnalyzerSettings settings = MethodAnalyzerSettings
+					.parseArgs(args);
+
+			// リポジトリを設定
+			SVNRepositoryManager.setup(settings.getRepositoryPath(),
+					settings.getAdditionalPath(), settings.getUserName(),
+					settings.getPasswd());
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 }
