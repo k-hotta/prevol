c6bb0c6f0561b8ff7acfcb4648727c7f02d419d7
k-hotta <k-hotta@ist.osaka-u.ac.jp>
2013-08-02 14:40:02 +0900

    create a class for a source file

--- src/jp/ac/osaka_u/ist/sdl/prevol/data/FileData.java
+++ src/jp/ac/osaka_u/ist/sdl/prevol/data/FileData.java
@@ -1,5 +1,7 @@
 package jp.ac.osaka_u.ist.sdl.prevol.data;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 /**
  * ファイルを表すクラス
  * 
@@ -7,5 +9,49 @@ package jp.ac.osaka_u.ist.sdl.prevol.data;
  * 
  */
 public class FileData extends AbstractElement {
+
+	/**
+	 * ID管理用カウンタ
+	 */
+	private static final AtomicLong count = new AtomicLong(0);
+
+	/**
+	 * このファイルの生存期間の先頭リビジョンのID
+	 */
+	private final long startRevisionId;
+
+	/**
+	 * このファイルの生存期間の最終リビジョンのID
+	 */
+	private final long endRevisionId;
+
+	/**
+	 * ファイルパス
+	 */
+	private final String path;
+
+	public FileData(final long startRevisionId, final long endRevisionId,
+			final String path) {
+		super(count.getAndIncrement());
+		this.startRevisionId = startRevisionId;
+		this.endRevisionId = endRevisionId;
+		this.path = path;
+	}
+
+	/*
+	 * ゲッタ群
+	 */
+
+	public final long getStartRevisionId() {
+		return startRevisionId;
+	}
+
+	public final long getEndRevisionId() {
+		return endRevisionId;
+	}
+
+	public final String getPath() {
+		return path;
+	}
 
 }
