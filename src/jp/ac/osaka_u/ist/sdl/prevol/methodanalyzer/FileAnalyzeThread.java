b50b52752396aaa81f45bd538746d98756013b1d
k-hotta <k-hotta@ist.osaka-u.ac.jp>
2013-08-02 16:42:09 +0900

    tailor analyzers for files to match the structure change in previous commits

--- src/jp/ac/osaka_u/ist/sdl/prevol/methodanalyzer/FileAnalyzeThread.java
+++ src/jp/ac/osaka_u/ist/sdl/prevol/methodanalyzer/FileAnalyzeThread.java
@@ -1,12 +1,12 @@
 package jp.ac.osaka_u.ist.sdl.prevol.methodanalyzer;
 
-import java.util.Collection;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import jp.ac.osaka_u.ist.sdl.prevol.data.FileData;
 import jp.ac.osaka_u.ist.sdl.prevol.data.MethodData;
 import jp.ac.osaka_u.ist.sdl.prevol.data.RevisionData;
+import jp.ac.osaka_u.ist.sdl.prevol.data.VectorData;
 import jp.ac.osaka_u.ist.sdl.prevol.methodanalyzer.svn.SVNRepositoryManager;
 
 import org.eclipse.jdt.core.dom.CompilationUnit;
@@ -40,20 +40,36 @@ public class FileAnalyzeThread implements Runnable {
 	private final AtomicInteger index;
 
 	/**
-	 * 検出されたメソッドを保持するためのマップ <br>
-	 * キーはファイル，値はそのファイルに含まれるメソッドのコレクション
+	 * 解析したファイルを保持するためのマップ <br>
+	 * キーはID，値はファイル
 	 */
-	private final ConcurrentMap<FileData, Collection<MethodData>> detectedMethods;
+	private final ConcurrentMap<Long, FileData> analyzedFiles;
+
+	/**
+	 * 検出されたメソッドを保持するためのマップ <br>
+	 * キーはID，値はメソッド
+	 */
+	private final ConcurrentMap<Long, MethodData> detectedMethods;
+
+	/**
+	 * 検出されたベクトルデータを保持するためのマップ <br>
+	 * キーはID，値はベクトルデータ
+	 */
+	private final ConcurrentMap<Long, VectorData> detectedVectors;
 
 	public FileAnalyzeThread(final String[] targets,
 			final RevisionData revision, final RevisionData latestRevision,
 			final AtomicInteger index,
-			ConcurrentMap<FileData, Collection<MethodData>> detectedMethods) {
+			final ConcurrentMap<Long, FileData> analyzedFiles,
+			final ConcurrentMap<Long, MethodData> detectedMethods,
+			final ConcurrentMap<Long, VectorData> detectedVectors) {
 		this.targets = targets;
 		this.revision = revision;
 		this.latestRevision = latestRevision;
 		this.index = index;
+		this.analyzedFiles = analyzedFiles;
 		this.detectedMethods = detectedMethods;
+		this.detectedVectors = detectedVectors;
 	}
 
 	@Override
@@ -84,7 +100,9 @@ public class FileAnalyzeThread implements Runnable {
 				rootNode.accept(analyzer);
 
 				// 結果を保存
-				detectedMethods.put(file, analyzer.getMethods());
+				analyzedFiles.put(file.getId(), file);
+				detectedMethods.putAll(analyzer.getMethods());
+				detectedVectors.putAll(analyzer.getVectors());
 
 			} catch (Exception e) {
 				e.printStackTrace();
--- src/jp/ac/osaka_u/ist/sdl/prevol/methodanalyzer/FilesAnalyzer.java
+++ src/jp/ac/osaka_u/ist/sdl/prevol/methodanalyzer/FilesAnalyzer.java
@@ -10,6 +10,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 import jp.ac.osaka_u.ist.sdl.prevol.data.FileData;
 import jp.ac.osaka_u.ist.sdl.prevol.data.MethodData;
 import jp.ac.osaka_u.ist.sdl.prevol.data.RevisionData;
+import jp.ac.osaka_u.ist.sdl.prevol.data.VectorData;
 import jp.ac.osaka_u.ist.sdl.prevol.methodanalyzer.svn.RepositoryNotInitializedException;
 
 import org.tmatesoft.svn.core.SVNException;
@@ -42,12 +43,34 @@ public class FilesAnalyzer {
 	 */
 	private final int threadsCount;
 
+	/**
+	 * 解析したファイルを保持するためのマップ <br>
+	 * キーはID，値はファイル
+	 */
+	private final ConcurrentMap<Long, FileData> analyzedFiles;
+
+	/**
+	 * 検出されたメソッドを保持するためのマップ <br>
+	 * キーはID，値はメソッド
+	 */
+	private final ConcurrentMap<Long, MethodData> detectedMethods;
+
+	/**
+	 * 検出されたベクトルデータを保持するためのマップ <br>
+	 * キーはID，値はベクトルデータ
+	 */
+	private final ConcurrentMap<Long, VectorData> detectedVectors;
+
 	public FilesAnalyzer(final String[] targets, final RevisionData revision,
 			final RevisionData latestRevision, final int threadsCount) {
 		this.targets = targets;
 		this.revision = revision;
 		this.latestRevision = latestRevision;
 		this.threadsCount = threadsCount;
+		this.analyzedFiles = new ConcurrentHashMap<Long, FileData>();
+		this.detectedMethods = new ConcurrentHashMap<Long, MethodData>();
+		this.detectedVectors = new ConcurrentHashMap<Long, VectorData>();
+
 	}
 
 	public FilesAnalyzer(final Collection<String> targets,
@@ -58,21 +81,48 @@ public class FilesAnalyzer {
 	}
 
 	/**
-	 * ファイル群をマルチスレッドで解析
+	 * 解析したファイルを取得
 	 * 
 	 * @return
+	 */
+	public final Map<Long, FileData> getAnalyzedFiles() {
+		return Collections.unmodifiableMap(analyzedFiles);
+	}
+
+	/**
+	 * 検出されたメソッドを取得
+	 * 
+	 * @return
+	 */
+	public final Map<Long, MethodData> getDetectedMethods() {
+		return Collections.unmodifiableMap(detectedMethods);
+	}
+
+	/**
+	 * 検出されたベクトルデータを取得
+	 * 
+	 * @return
+	 */
+	public final Map<Long, VectorData> getDetectedVectors() {
+		return Collections.unmodifiableMap(detectedVectors);
+	}
+
+	/**
+	 * ファイル群をマルチスレッドで解析 <br>
+	 * これの実行が終わったら各フィールドに解析結果が保存される
+	 * 
 	 * @throws RepositoryNotInitializedException
 	 * @throws SVNException
 	 */
-	public Map<FileData, Collection<MethodData>> analyzeFiles()
-			throws RepositoryNotInitializedException, SVNException {
+	public void analyzeFiles() throws RepositoryNotInitializedException,
+			SVNException {
 		final AtomicInteger index = new AtomicInteger(0);
-		final ConcurrentMap<FileData, Collection<MethodData>> detectedMethods = new ConcurrentHashMap<FileData, Collection<MethodData>>();
 
 		final Thread[] threads = new Thread[threadsCount];
 		for (int i = 0; i < threads.length; i++) {
 			threads[i] = new Thread(new FileAnalyzeThread(targets, revision,
-					latestRevision, index, detectedMethods));
+					latestRevision, index, analyzedFiles, detectedMethods,
+					detectedVectors));
 			threads[i].start();
 		}
 
@@ -84,7 +134,6 @@ public class FilesAnalyzer {
 			}
 		}
 
-		return Collections.unmodifiableMap(detectedMethods);
 	}
 
 }
