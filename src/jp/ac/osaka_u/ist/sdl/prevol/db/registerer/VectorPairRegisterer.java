71ed779edebbbf4bd2573a9ff530f1962df6915f
k-hotta <k-hotta@ist.osaka-u.ac.jp>
2013-08-03 20:08:01 +0900

    create a registerer for VectorPairData

---
+++ src/jp/ac/osaka_u/ist/sdl/prevol/db/registerer/VectorPairRegisterer.java
@@ -0,0 +1,12 @@
+package jp.ac.osaka_u.ist.sdl.prevol.db.registerer;
+
+import jp.ac.osaka_u.ist.sdl.prevol.data.VectorPairData;
+import jp.ac.osaka_u.ist.sdl.prevol.db.DBConnection;
+
+public class VectorPairRegisterer extends AbstractElementRegisterer<VectorPairData> {
+
+	public VectorPairRegisterer(DBConnection connection) {
+		super(connection);
+	}
+
+}
